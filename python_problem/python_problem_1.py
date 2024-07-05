53f273c69a42d1338e8f133a221f2054fbebbf7a
yumin22 <22joomin@naver.com>
2024-07-06 02:24:13 +0900

    6단계 구현

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,46 +1,50 @@
 import random
 num=0
+number=1
+player = 'A'
 
-num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+while number <=31:
 
-try: 
-    int_num = int(num)
-except ValueError:
-    print("정수를 입력하세요")
-    num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-
-
-try:
-    num = int(num)
-    if num < 1 or num >3:
-        print("1,2,3 중 하나를 입력하세요")
-        num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-except ValueError:
-    pass
-
-playerA_number = 1
-if num > 0:
-    for i in range(num):
-        print(f"playerA : {playerA_number}")
-        playerA_number += 1
-
-
-while True:
     num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
 
     try: 
         num=int(num)
         if num not in [1, 2, 3]:
-                print("1, 2, 3 중 하나를 입력하세요")
+            print("1, 2, 3 중 하나를 입력하세요")
+            continue
 
-        else:
-            break
     except ValueError:
         print("정수를 입력하세요")
+        continue
+
+
+    for i in range(num):
+        print(f"player {player} : {number}")
+        number += 1
+
+        if number > 31:
+            break
+
+        
+        if player == 'A':
+            player = 'B'
+        else:
+            player= 'A'
+
+
+    #while True:
+        #num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+
+        #try: 
+            #num=int(num)
+            #if num not in [1, 2, 3]:
+                    #print("1, 2, 3 중 하나를 입력하세요")
+                    #continue
+
+        #except ValueError:
+            #print("정수를 입력하세요")
+            #continue
 
 
 
-playerB_number = playerA_number
-for i in range(num):
-    print(f"playerB : {playerB_number}")
-    playerB_number += 1
+
