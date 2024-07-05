f412d4dff3789fafdb11d2d05fbe9619dc629787
yumin22 <22joomin@naver.com>
2024-07-06 03:04:23 +0900

    8단계 완료

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,41 +1,38 @@
 import random
-num=0
-number=1
-player = 'A'
 
-while number <=31:
+def brGame(player):
+    number = 1
+    while number <= 31:
+        num = input(f"player{player}, 부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+        
+        try: 
+            num = int(num)
+            if num not in [1, 2, 3]:
+                print("1, 2, 3 중 하나를 입력하세요")
+                continue
 
-    num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-
-    try: 
-        num=int(num)
-        if num not in [1, 2, 3]:
-            print("1, 2, 3 중 하나를 입력하세요")
+        except ValueError:
+            print("정수를 입력하세요")
             continue
 
-    except ValueError:
-        print("정수를 입력하세요")
-        continue
+        for i in range(num):
+            print(f"player{player} : {number}")
+            number += 1
 
-
-    for i in range(num):
-        print(f"player{player} : {number}")
-        number += 1
-
-        if number > 31:
-            break
-        
+            if number > 31:
+                return player
         
         if player == 'A':
             player = 'B'
         else:
-            player= 'A'
+            player = 'A'
 
-winner = 'B' if player == 'A' else 'A'
+    return player
+
+player = 'A'
+brGame(player)
+winner = 'B' if player=='A' else 'A'
 print(f"player{winner} win!")
-
-
-    
 
 
 
