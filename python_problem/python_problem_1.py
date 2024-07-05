bfcd8df0c43721a2494034969cbb3751813feb24
yumin22 <22joomin@naver.com>
2024-07-06 02:05:17 +0900

    5단계 완료

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -18,9 +18,29 @@ try:
 except ValueError:
     pass
 
-try: 
-    num = int(num)
-    for i in range(1, num+1):
-        print(f"playerA: {i}")
-except ValueError:
-    pass
+playerA_number = 1
+if num > 0:
+    for i in range(num):
+        print(f"playerA : {playerA_number}")
+        playerA_number += 1
+
+
+while True:
+    num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+
+    try: 
+        num=int(num)
+        if num not in [1, 2, 3]:
+                print("1, 2, 3 중 하나를 입력하세요")
+
+        else:
+            break
+    except ValueError:
+        print("정수를 입력하세요")
+
+
+
+playerB_number = playerA_number
+for i in range(num):
+    print(f"playerB : {playerB_number}")
+    playerB_number += 1
