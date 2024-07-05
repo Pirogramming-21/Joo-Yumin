bbb15cf7bba26f54a38c26e3a7e7bd3ac4323048
yumin22 <22joomin@naver.com>
2024-07-06 02:29:10 +0900

    7단계 완료

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -19,31 +19,23 @@ while number <=31:
 
 
     for i in range(num):
-        print(f"player {player} : {number}")
+        print(f"player{player} : {number}")
         number += 1
 
         if number > 31:
             break
-
+        
         
         if player == 'A':
             player = 'B'
         else:
             player= 'A'
 
+winner = 'B' if player == 'A' else 'A'
+print(f"player{winner} win!")
 
-    #while True:
-        #num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
 
-        #try: 
-            #num=int(num)
-            #if num not in [1, 2, 3]:
-                    #print("1, 2, 3 중 하나를 입력하세요")
-                    #continue
-
-        #except ValueError:
-            #print("정수를 입력하세요")
-            #continue
+    
 
 
 
