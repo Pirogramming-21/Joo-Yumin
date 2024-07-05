a5231d68700e44bc49eddda434bed6c95243a010
yumin22 <22joomin@naver.com>
2024-07-06 01:50:57 +0900

    4단계 완료

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,18 +1,26 @@
+import random
 num=0
 
-number = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
 
 try: 
-    int_number = int(number)
+    int_num = int(num)
 except ValueError:
     print("정수를 입력하세요")
-    number = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+    num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
 
 
 try:
-    number = int(number)
-    if number < 1 or number >3:
+    num = int(num)
+    if num < 1 or num >3:
         print("1,2,3 중 하나를 입력하세요")
-        number = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+        num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+except ValueError:
+    pass
+
+try: 
+    num = int(num)
+    for i in range(1, num+1):
+        print(f"playerA: {i}")
 except ValueError:
     pass
