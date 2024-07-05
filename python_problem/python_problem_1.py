6f2a20936890808beca8232f9d02688b8464b6aa
yumin22 <22joomin@naver.com>
2024-07-06 01:42:28 +0900

    3단계 완료

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,4 +1,18 @@
 num=0
 
 number = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-print(number)
+
+try: 
+    int_number = int(number)
+except ValueError:
+    print("정수를 입력하세요")
+    number = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+
+
+try:
+    number = int(number)
+    if number < 1 or number >3:
+        print("1,2,3 중 하나를 입력하세요")
+        number = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+except ValueError:
+    pass
