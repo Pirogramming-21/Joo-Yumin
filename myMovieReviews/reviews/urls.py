4ae5d71b3bd57e41714a080f42ddeacdc4fab9d1
yumin22 <22joomin@naver.com>
2024-07-12 14:34:23 +0900

    [feat] 기능1 구현

--- myMovieReviews/reviews/urls.py
+++ myMovieReviews/reviews/urls.py
@@ -4,5 +4,6 @@ from .views import *
 app_name = 'MovieReviews'
 
 urlpatterns = [
-    path('', list, name='list'),
+    path('', list, name='review_list'),
+    path('/create', create, name='review_create')
 ]
--- myMovieReviews/reviews/views.py
+++ myMovieReviews/reviews/views.py
@@ -4,7 +4,19 @@ from .models import Review
 # Create your views here.
 def list(request):
     reviews = Review.objects.all()
-    context = {
-        "reviews":reviews
-    }
-    return render(request, 'review_list.html')
+    return render(request, 'review_list.html', {'reviews': reviews})
+
+def create(request):
+    if request.method == "POST":
+        Review.objects.create(
+            title = request.POST["title"],
+            release = request.POST["release"],
+            director = request.POST["director"],
+            actor = request.POST["actor"],
+            genre = request.POST["genre"],
+            score = request.POST["score"],
+            running_time = request.POST["running_time"],
+            content = request.POST["content"]
+        )
+        return redirect("/")
+    return render(request, 'review_create.html')
