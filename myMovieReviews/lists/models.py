0917878856cb8aa21ede7c27221c03432eaf7624
yumin22 <22joomin@naver.com>
2024-07-12 12:25:03 +0900

    [fix] List model -> Review model 변경 중

--- myMovieReviews/lists/models.py
+++ myMovieReviews/lists/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class List(models.Model):
+class Review(models.Model):
     title = models.CharField(max_length=30)
-    user = models.CharField(max_length=20)
+    release = models.IntegerField() 
+    director = models.CharField(max_length=100)  
+    actor = models.CharField(max_length=100)  
+    genre = models.CharField(max_length=50) 
+    score = models.FloatField()  
+    runningTime = models.IntegerField()  
     content = models.TextField()
-
--- myMovieReviews/lists/urls.py
+++ myMovieReviews/lists/urls.py
@@ -5,4 +5,5 @@ app_name = 'list'
 
 urlpatterns = [
     path('', movie_list, name='movie_list'),
+    path('lists/create/', review_create, name='review_create'),
 ]
