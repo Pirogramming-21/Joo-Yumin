4d28a72b73683244180cb6baf43c1b59667e8644
yumin22 <22joomin@naver.com>
2024-07-18 05:20:24 +0900

    [feat] 기능 추가 구현

--- SWIDEA_SITE/apps/devTools/views.py
+++ SWIDEA_SITE/apps/devTools/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import DevTool
 from .forms import DevToolForm
+from apps.posts.models import Post
 
 # Create your views here.
 
@@ -15,27 +16,31 @@ def create(request):
     return redirect("posts:main")
 
 def list(request):
-    tools = DevTool.objects.all()
-    ctx = {'tools':tools}
-    return render(request, 'devTools/list.html', ctx )
+    devTools = DevTool.objects.all()
+    ctx = {'devTools': devTools}
+    return render(request, 'devTools/list.html', ctx)
 
 def delete(request, pk):
     DevTool.objects.get(id=pk).delete()
-    return redirect('posts:main')
+    return redirect('devTools:list')
 
-def update(request, pk):
+def update(request,pk):
     tool = DevTool.objects.get(id=pk)
-    if request.method == "GET":
+    if request.method == 'GET':
         form = DevToolForm(instance=tool)
-        ctx = {'form':form, 'pk':pk}
+        ctx = {'form' : form, 'pk':pk}
         return render(request, 'devTools/update.html', ctx)
-    form = DevToolForm(request.POST, request.FILES, instance=tool)
+    
+    form = DevToolForm(request.POST, instance=tool)
     if form.is_valid():
         form.save()
-    return redirect('devTools:detail', pk)
+        return redirect('devTools:list')
+    ctx = {'form': form, 'pk': pk}
+    return render(request, 'devTools/update.html', ctx)
 
 def detail(request, pk):
-    tool = DevTool.objects.get(id=pk)
-    ctx = {'tool':tool}
+    devTool = DevTool.objects.get(id=pk)  # 여기서 devTool 객체를 가져옵니다.
+    posts = Post.objects.filter(devtool_id=devTool.id)  # DevTool 객체의 ID로 필터링합니다.
+    ctx = {'devTool': devTool, 'posts': posts}
     return render(request, 'devTools/detail.html', ctx)
 
